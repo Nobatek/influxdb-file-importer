a2086c1cf73d7ea07b509c431ceb726406fffbaf
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-12-02 17:09:13 +0100

    Bump version: 0.1.0 → 0.1.1

--- influxdb_file_importer.py
+++ influxdb_file_importer.py
@@ -8,7 +8,7 @@ import json
 import influxdb_client
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 class InfluxDBFileImporter(abc.ABC):
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='influxdb-file-importer',
-    version='0.1.0',
+    version='0.1.1',
     description='Import data from files into InfluxDB',
     long_description=long_description,
     url='https://github.com/Nobatek/influxdb-file-importer',
